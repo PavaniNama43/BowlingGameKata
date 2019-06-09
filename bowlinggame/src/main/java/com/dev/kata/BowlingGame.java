451914012b67e90edc479c681d95cdacf56ccffb
sri <srinidhi.aislin@gmail.com>
2019-06-09 19:53:21 +0200

    GameScore when no pins are knocked for 20 rolls

---
+++ bowlinggame/src/main/java/com/dev/kata/BowlingGame.java
@@ -0,0 +1,12 @@
+package com.dev.kata;
+
+public class BowlingGame {
+
+	public void roll(int i) {
+	}
+
+	public int getScore() {
+		return 0;
+	}
+
+}
--- bowlinggame/src/test/java/com/dev/kata/BowlingGameTest.java
+++ bowlinggame/src/test/java/com/dev/kata/BowlingGameTest.java
@@ -1,12 +1,17 @@
 package com.dev.kata;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class BowlingGameTest {
 	@Test
-	public void validateSetUp() {
-		assertTrue(true);
+	public void testGameScoreForGutterBalls() {
+		BowlingGame game = new BowlingGame();
+		
+		for(int i=0;i<20;i++) {
+			game.roll(0);
+		}
+		
+		assertEquals(0, game.getScore());
 	}
 }
